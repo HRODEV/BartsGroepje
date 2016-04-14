7006b974763cf227da2446131265a5a4c6be5942
rick droogh <rak_keeper@hotmail.com>
2016-04-14 17:04:35 +0200

    added first things of charting

--- Rapport/Rapport/Program.fs
+++ Rapport/Rapport/Program.fs
@@ -1,8 +1,18 @@
 ﻿open System
 open System.Data
 open System.Data.Common
+open FSharp.Data
 open FSharp.Charting
 open System.Windows.Forms
+
+// let file = System.IO.File.ReadAllText(@"System.Environment.CurrentDirectory\file.csv")
+
+(*let data = CsvProvider<"/Data/CountStopsPerHourInDays.csv">.GetSample()
+
+data.Rows |> Seq.map (fun row -> row.)
+*)
+
+printfn "%A" data
 
 let chart = Chart.Line([ for x in 0 .. 10 -> x, x*x ]).ShowChart()
 
