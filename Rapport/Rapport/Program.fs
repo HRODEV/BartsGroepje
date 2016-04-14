0cba347de7f64a980ca7a1599a9c5df30fed6088
rick droogh <rak_keeper@hotmail.com>
2016-04-14 16:19:24 +0200

    Created chart F# project

---
+++ Rapport/Rapport/Program.fs
@@ -0,0 +1,9 @@
+﻿open System
+open System.Data
+open System.Data.Common
+open FSharp.Charting
+open System.Windows.Forms
+
+let chart = Chart.Line([ for x in 0 .. 10 -> x, x*x ]).ShowChart()
+
+Application.Run(chart)
