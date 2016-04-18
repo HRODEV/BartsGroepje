24dcb79b39d3bd63dc4ebe8d54f68cb1ce73aed8
rick droogh <rak_keeper@hotmail.com>
2016-04-18 16:20:32 +0200

    added text

--- Rapport/Rapport/Line_Has_Stations_Chart_Gen.fs
+++ Rapport/Rapport/Line_Has_Stations_Chart_Gen.fs
@@ -10,6 +10,6 @@ open System.Windows.Forms
 let GetChart =
     let data = CsvProvider<"Data/stationsOnLine.csv">.GetSample()
 
-    let chart = Chart.Bar(data.Rows |> Seq.map (fun row -> row.Line, row.Count)).WithLegend(true, "Legenda")
+    let chart = Chart.Bar(data.Rows |> Seq.map (fun row -> row.Line, row.Count), "", "Aantal stations per lijn")
 
     (chart, "LineHasStations")
