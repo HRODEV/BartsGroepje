83f224e717563572e4d93b188c95b9a41def26ff
Damien Rietdijk <damien59881@gmail.com>
2016-04-19 12:27:39 +0200

    Revert "Return unit"
    This reverts commit 6cf07df15e0d96366b06c57eef99185cdf405d16.

--- Project3/RealtimeDataFSharp/Coroutines.fs
+++ Project3/RealtimeDataFSharp/Coroutines.fs
@@ -5,26 +5,26 @@ type Coroutine<'a, 's> = 's -> CoroutineStep<'a, 's>
     |   Done of 'a * 's
     |   Wait of Coroutine<'a, 's> * 's
 
-let rec (>>=) p k =
+let rec (>>) p k =
     fun s ->
         match p s with
         | Done(a, s') -> k a s'
-        | Wait(leftOver, s') -> Wait((leftOver >>= k), s')
+        | Wait(leftOver, s') -> Wait((leftOver >> k), s')
 
 type CoroutineBuilder() = 
     member this.Return(x) = fun s -> Done(x, s)
-    member this.Bind(p, k) = p >>= k
-    member this.ReturnFrom(c) = c
+    member this.Bind(p, k) = p >> k
+    member this.ReturnFrom c = c
+    member this.Zero () = fun s -> Done((), s)
 
 let co = CoroutineBuilder()
 
 let getState = fun s -> Done(s, s)
 let yield_ = fun s -> Wait((fun s -> Done((), s)), s)
-
 let rec repeat_ c =
     co {
         do! c
-        return! repeat_ 
+        return! repeat_ c
     }
 
 let rec costep coroutine state =
